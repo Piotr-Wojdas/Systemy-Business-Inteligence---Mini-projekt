376c628aba7eae2d064b91d6eb8911f927e61f42
Dolomirr <dolomirr@gmail.com>
2026-06-09 23:40:22 +0200

    updated migraion script

--- migrate.py
+++ migrate.py
@@ -8,31 +8,39 @@ load_dotenv()
 
 
 def migrate_pg_to_mssql():
-    pg_user = os.getenv("POSTGRES_USER")
-    pg_password = os.getenv("POSTGRES_PASSWORD")
-    pg_db = os.getenv("POSTGRES_DB")
-    pg_port = os.getenv("POSTGRES_PORT", "5432")
-
-    pg_url = f"postgresql://{pg_user}:{pg_password}@localhost:{pg_port}/{pg_db}"
+    pg_url = (
+        f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
+        f"@localhost:{os.getenv('POSTGRES_PORT', '5432')}/{os.getenv('POSTGRES_DB')}"
+    )
 
     mssql_url = (
-        f"mssql+pyodbc://{ms_user}:{ms_password}@{ms_host}:1433/{ms_db}?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=no"
+        f"mssql://sa:{os.getenv('MSSQL_SA_PASSWORD')}@localhost:{os.getenv('MSSQL_PORT', '1433')}/master"
+        f"?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes&Encrypt=yes"
     )
 
     postgres_source = sql_database(
         credentials=pg_url,
         schema="olap",
+        table_names=[
+            "dim_category",
+            "dim_date",
+            "dim_payment_type",
+            "dim_time",
+            "dim_vendor",
+            "dim_weather",
+            "dim_zone",
+            "fact_trip",
+        ],
     )
 
     pipeline = dlt.pipeline(
         pipeline_name="postgres_to_mssql",
-        destination="mssql",
-        credentials=mssql_url,
+        destination=dlt.destinations.mssql(mssql_url),
         dataset_name="olap",
+        progress="enlighten",
     )
 
-    load_info = pipeline.run(postgres_source, write_disposition="replace")
-    print(load_info)
+    print(pipeline.run(postgres_source, write_disposition="replace"))
 
 
 if __name__ == "__main__":
